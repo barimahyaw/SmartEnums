8ff53547d53aa78b34eed525a7705baf84eb6fe1
Isaac Nana Gyan Ayeh <nanabarimah22@gmail.com>
2024-01-04 18:02:06 +0000

    Add project files.

---
+++ SmartEnums/CreditCard.cs
@@ -0,0 +1,43 @@
+﻿namespace SmartEnums;
+
+public abstract class CreditCard : Enumeration<CreditCard>
+{ 
+    public static readonly CreditCard Standard = new StandardCreditCard();
+    public static readonly CreditCard Premium = new PremiumCreditCard();
+    public static readonly CreditCard Platinum = new PlatinumCreditCard();
+
+    private CreditCard(int value, string name)
+       : base(value, name)
+    {
+    }
+
+    public abstract double Discount { get; }
+
+    public sealed class StandardCreditCard 
+        : CreditCard
+    {
+        public StandardCreditCard() 
+            : base(1, nameof(Standard))
+        {
+        }
+        public override double Discount => 0.01;
+    }
+
+    public sealed class PremiumCreditCard 
+        : CreditCard
+    {
+        public PremiumCreditCard()
+            : base(2, nameof(Premium))
+        {
+        }
+        public override double Discount => 0.02;
+    }
+
+    public sealed class PlatinumCreditCard : CreditCard
+    {
+        public PlatinumCreditCard() : base(3, nameof(Platinum))
+        {
+        }
+        public override double Discount => 0.03;
+    }
+}
---
+++ SmartEnums/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SmartEnums;
+
+Console.WriteLine("Hello, World!");
+
+var creditCard = CreditCard.FromValue(1);
+Console.WriteLine($"Credit card: {creditCard} with discount of {creditCard?.Discount}");
+
+var creditCardName = CreditCard.FromName(nameof(CreditCard.Platinum));
+Console.WriteLine($"Credit card: {creditCardName} with discount of {creditCardName?.Discount}");
